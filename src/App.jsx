826233acbd88d97d715907dec7b17a1ab35efb79
aadarshantony <adarshantony223@gmail.com>
2026-02-20 04:24:43 +0530

    dumb2

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,10 @@ import LandingPage from './pages/LandingPage';
 import CitizenPortal from './pages/CitizenPortal'; // Import the new page
 
 // Remaining Placeholder Components
-const AdminDash = () => <div className="p-20 text-center">Admin Dashboard Content</div>;
+import AdminDashboard from './pages/AdminDashboard';
+import RationShopDealer from './pages/RationShopDealer'
+import WarehousePanel from './pages/WarehousePanel'
+
 const WarehouseDash = () => <div className="p-20 text-center">Warehouse Management Content</div>;
 const DealerDash = () => <div className="p-20 text-center">Ration Shop Dealer Content</div>;
 
@@ -12,10 +15,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/admin" element={<AdminDash />} />
-        <Route path="/warehouse" element={<WarehouseDash />} />
-        <Route path="/dealer" element={<DealerDash />} />
-        <Route path="/citizen" element={<CitizenPortal />} />
       </Routes>
     </Router>
   );
